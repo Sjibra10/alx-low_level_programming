87e19c79d9952eabfb04ff5e7a218dac7af2562d
Sjibra10 <buhleobasibuka@gmail.com>
2023-09-25 17:25:59 -0700

    go go

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers from 0 to 14 ten times
+*
+*/
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
