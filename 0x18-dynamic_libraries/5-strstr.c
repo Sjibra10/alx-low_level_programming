04e429256e4f6c5fc62981dae5a45dac38fbdfe4
‘Sjibra10’ <buhleobasibuka@gmail.com>
2024-01-17 22:24:35 +0200

    fixes

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -1,6 +1,16 @@
+#include "main.h"
+
+/**
+* _strstr - used to search a needle
+* @haystack: a pointer
+* @needle: a pointer
+* Return: empty
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
 	(void)haystack;
 	(void)needle;
 	return ("");
 }
+
